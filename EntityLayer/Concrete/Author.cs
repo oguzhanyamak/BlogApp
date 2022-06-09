fd738d0e8c1aa1d69d8f67981d875b6e05cc9ac0
Oğuzhan Yamak <oguzhanyamak@outlook.com>
2022-06-09 18:07:17 +0300

    Entitylerin Tanımlanması ve altyapı işlemleri

---
+++ EntityLayer/Concrete/Author.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Author
+    {
+        public int AuthorID { get; set; }
+        public string AuthorName { get; set; }
+        public string AuthorAbout { get; set; }
+        public string AuthorMail { get; set; }
+        public string AuthorPassword { get; set; }
+        public string AuthorImage { get; set; }
+        public bool AuthorStatus { get; set; }
+
+    }
+}
